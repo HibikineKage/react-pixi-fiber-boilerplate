fa67e138e58addeffe8e5e8bdecd86d92529cfd2
Hibikine Kage <goodsmile314159@gmail.com>
2018-05-13 05:15:44 +0900

    feat: add animation-sprite props

--- src/components/animation-sprite.tsx
+++ src/components/animation-sprite.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { Sprite } from 'react-pixi-fiber';
 
-interface Props {}
+interface Props {
+  alpha?: number;
+  x?: number;
+  y?: number;
+  animationSpeed?: number;
+
+  textures: PIXI.Texture[];
+}
 const AnimationSprite = (props: Props) => <Sprite />;
 export default AnimationSprite;
